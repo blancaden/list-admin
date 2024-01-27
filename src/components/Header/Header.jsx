c09397c24229f1bd19e586e33f42c9f1811d18f9
Blanca <blankdgonzfigue@gmail.com>
2024-01-27 10:39:16 +0100

    1er commit Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+
+import React from 'react'
+
+
+const Header = () => {
+  return (
+    <>
+
+   <header>
+        <section class="nav-bar">
+
+            <figure class="img-content">
+                <img src="img-logo/logo-ico.ico" alt="">
+            </figure>
+
+            <nav class="nav-content">
+                <button class="btn-log" id="logoutButton" type="button">Logout</button> 
+            </nav>
+            
+        </section>
+    </header>
+
+
+    </>
+  )
+}
+
+export default Header;
