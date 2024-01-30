41761b9392e444f287bd1bd54b362eeba200e347
Blanca <blankdgonzfigue@gmail.com>
2024-01-30 13:29:36 +0100

    UPDATE HomeView + PageAdmin

--- src/components/Views/PageAdmin/PageAdmin.jsx
+++ src/components/Views/PageAdmin/PageAdmin.jsx
@@ -1,13 +1,17 @@
+
 import React from 'react'
 import FormList from '../../FormList/FormList'
+import TableList from '../../TableList/TableList'
+import '../PageAdmin/PageAdmin.css'
 
 const PageAdmin = () => {
   return (
-    <>
+    < section className='page_body'>
     
-    <FormList />
-    
-    </>
+      <FormList />
+      <TableList />
+      
+    </section>
   )
 }
 
